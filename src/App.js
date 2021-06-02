bc8021270584d77831fa621dc7654199cedcac34
Juan carlos salcedo <juank9225@gmail.com>
2021-06-01 21:42:05 -0500

    Se agrega la funcion onEditar que nos permite editar los todos registrados

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React, {useContext, useReducer, useEffect, useRef,useState,createContext 
 
 const HOST_API= "http://localhost:8080/api";
 const initialState = {
-  list:[]
+  list:[],
+  item:{}
 }
 
 const Store = createContext(initialState)
 
 const Form = () => {
-  const {dispatch}=useContext(Store);
+  const {dispatch, state:{item}}=useContext(Store);
   const formRef = useRef(null);
-  const [state,setState] = useState({});
+  const [state,setState] = useState(item);
   
   const onAdd =(event)=>{
     event.preventDefault();
@@ -37,11 +38,39 @@ const Form = () => {
     });
   }
 
+  const onEdit =(event)=>{
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted
+    };
+
+    fetch(HOST_API+'/todo',{
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then(response =>response.json())
+    .then((todo)=>{
+      dispatch({type:"update-item", item:todo});
+      setState({name:""});
+      formRef.current.reset();
+    });
+  }
+
+
   return<form ref={formRef}>
-  <input type="text" name="name" onChange={(event)=>{
+  <input type="text" name="name" defaultValue={item.name} onChange={(event)=>{
     setState({...state, name: event.target.value})
   }}></input>
-  <button onClick={onAdd}>Agregar</button>
+  {item.id && <button onClick={onEdit}>Actualizar</button>}
+  {!item.id && <button onClick={onAdd}>Agregar</button>}
+  
+
 </form>
 }
 
@@ -55,6 +84,12 @@ const List =() =>{
       dispatch({type:"update-list",list})
     })
   },[state.list.length, dispatch]);
+
+  //falta Dele
+
+  const onEdit = (todo) =>{
+    dispatch({ type: "edit-item", item: todo})
+  };
   
   return<div>
     <table>
@@ -70,7 +105,9 @@ const List =() =>{
           return <tr key={todo.id}>
             <td>{todo.id}</td>
             <td>{todo.name}</td>
-            <td>{todo.isCompleted}</td>
+            <td>{todo.isCompleted ===true ? "SI" : "NO"}</td>
+            <td><button onClick={()=>onEdit(todo)}>Editar</button></td>
+
           </tr>
         })}
       </tbody>
@@ -80,8 +117,18 @@ const List =() =>{
 
 function reducer(state,action){
   switch(action.type){
+    case 'update-item':
+      const listUpdateEdit = state.list.map((item)=>{
+        if(item.id === action.item.id){
+              return action.item;
+        }
+        return item;
+      });
+      return {...state, list:listUpdateEdit, item:{}}
     case 'update-list':
       return{...state,list: action.list}
+    case 'edit-item':
+      return{...state,item: action.item}
     case 'add-item':
       const newList = state.list;
       newList.push(action.item);
